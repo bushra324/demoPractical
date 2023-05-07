8d9449bf48c36382730213838e4272f6f20dbb8d
Ahmad Zaki <shaikhzaki8@gmail.com>
2023-05-07 20:44:29 +0530

    demo practice add changes

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Sanila",
+                Gender = "Female",
+            };
+            return View(student);
+
+
+        }
+    }
+}
